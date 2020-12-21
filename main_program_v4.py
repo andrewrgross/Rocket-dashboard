56b3ae16e4a00f3b09c08b34bcb542f1ad85d925
AndrewGross <arg5029@gmail.com>
2020-12-21 10:51:22 -1000

    Lots of small updates

---
+++ main_program_v4.py
@@ -0,0 +1,311 @@
+### main_program_v3.py -- 12-02-2020 -- Andrew R Gross
+
+### Libraries
+import pygame
+import time
+import cv2
+import RPi.GPIO as GPIO
+import glob
+import numpy as np
+from ffpyplayer.player import MediaPlayer
+import vlc
+import io,sys,os,subprocess
+#from tkFileDialog import askopenfilename
+global process
+#name= askopenfilename(filetypes=[("Video Files","*.h264")])
+#subprocess.call(['vlc',name,'--play-and-exit'])
+#from os import listdir
+#from os.path import isfile, join
+
+pygame.init()
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen.fill((255, 255, 255))
+# Draw a solid blue circle in the center
+pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+### Declare variables and assets
+## Images
+menucontrol = pygame.image.load('/home/pi/Rocket-dashboard/Images/menu-control.png')
+menuradio = pygame.image.load('/home/pi/Rocket-dashboard/Images/menu-radio.png')
+win_space = pygame.image.load('/home/pi/Rocket-dashboard/Images/win-deepspace.png')
+win_mars = pygame.image.load('/home/pi/Rocket-dashboard/Images/win-mars.png')
+win_orbit = pygame.image.load('/home/pi/Rocket-dashboard/Images/win-orbit.png')
+win_vents = pygame.image.load('/home/pi/Rocket-dashboard/Images/win-vents.png')
+win_hole = pygame.image.load('/home/pi/Rocket-dashboard/Images/win-observatory.png')
+## Sounds
+beep1 = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/beep1.wav')
+beep2 = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/beep2.wav')
+bip = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/bip.wav')
+biz = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/biz.wav')
+bloop = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/bloop.wav')
+blorp = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/blorp.wav')
+boop = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/boop.wav')
+buzz = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/buzz.wav')
+chirp = pygame.mixer.Sound('/home/pi/Rocket-dashboard/Sounds/chirp.wav')
+
+### Add a system for sorting just the most recent for videos
+
+## Pins
+button1 = 2
+button2 = 3
+button3 = 25
+#button4
+switch1 = 4
+switch2 = 17
+switch3 = 27
+switch4 = 22
+## LEDs
+led1 = 9
+led2 = 1
+led3 = 1
+led4 = 1
+
+GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+GPIO.setup(button1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button3,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button4,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch3,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch4,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(led1,GPIO.OUT)
+
+### Starting parameters
+screenstate = 0
+
+switch1state = True
+switch2state = True
+switch3state = True
+switch4state = True
+light1color = (230,240,230)
+light2color = (230,230,240)
+light3color = (240,230,230)
+switch4color = (130, 130, 130)
+rollingvalue = 20
+modifier = 1
+font = pygame.font.SysFont(None, 66)
+font2 = pygame.font.SysFont(None, 50)
+
+window_list = (win_orbit, win_mars, win_space, win_hole, win_vents)
+current_window = 1
+
+### Functions
+
+def get_video_duration(path):
+	vidcapture = cv2.VideoCapture(path)
+	fps = vidcapture.get(cv2.CAP_PROP_FPS)
+	totalNoFrames = vidcapture.get(cv2.CAP_PROP_FRAME_COUNT);
+	durationInSeconds = float(totalNoFrames) / float(fps)
+	return(durationInSeconds)
+
+print('Setup complete. Running test')
+
+screen.blit(menucontrol, (0,0))
+pygame.display.update()
+
+## Videos
+#time.sleep(3)
+full_video_list = glob.glob('/home/pi/Rocket-dashboard/Videos/*')
+full_video_list.sort(key=os.path.getmtime, reverse = True)
+video_list = full_video_list[0:4]
+print(video_list)
+
+color_list = ((255,0,255), (255,0,127), (255,0,0), (255,127,0))
+text1 = font.render(video_list[0].split('/')[-1], True, color_list[0])
+text2 = font2.render(video_list[1].split('/')[-1], True, color_list[1])
+text3 = font2.render(video_list[2].split('/')[-1], True, color_list[2])
+text4 = font2.render(video_list[3].split('/')[-1], True, color_list[3])
+
+
+try:
+        while True:
+#               time.sleep(0.1)
+                ### Button1: Select
+                if GPIO.input(button1) == False:
+                        beep1.play()
+                        print('b1 pressed')
+                        if GPIO.input(button2) == False:
+                                print('button1-2 pressed')
+                                pygame.quit()
+                                quit()
+                        elif screenstate == 0:	# If on the control-menu screen:
+                                print('ss = 0')
+				### Draw dashboard
+                                screen.fill((255,255,250))
+                                pygame.draw.circle(screen, (230,255,230), (130,370), 50)
+                                pygame.draw.circle(screen, (  0,  0,  0), (130,370), 51, 15) # Ring
+                                pygame.draw.circle(screen, (255,230,210), (370,370), 50)
+                                pygame.draw.circle(screen, (  0,  0,  0), (370,370), 51, 15)
+                                pygame.draw.circle(screen, (255,230,210), (610,370), 50)
+                                pygame.draw.circle(screen, (  0,  0,  0), (610,370), 51, 15)
+                                pygame.draw.rect(screen, (100,100,100), (50,300,640,150), 26)
+                                screen.blit(win_orbit,(30,7))
+                                screenstate = 2
+                                time.sleep(0.6)
+                        elif screenstate == 1:	# If on the radio menu screen:
+                                print('Display list of videos')
+                                screen.fill((10,10,10))
+                                print('debug1')
+                                screen.blit(text1, (30, 50))
+                                screen.blit(text2, (60, 160))
+                                screen.blit(text3, (60, 260))
+                                screen.blit(text4, (60, 360))
+                                pygame.draw.rect(screen, (230,230,200), (10,10,640,130), 9)
+                                print('Set screen state to "3"')
+                                screenstate = 3
+                        elif screenstate == 2:		# If on the dashboard, go back to the control menu
+                                screen.blit(menucontrol, (0,0))
+                                screenstate = 0
+                        elif screenstate == 3:		# If on the radio select screen, play selected video
+                                pygame.quit()
+                                print('Playing '+video_list[0])
+                                durationInSeconds = get_video_duration(video_list[0])
+                                print(str(durationInSeconds) + ' seconds')
+     #                           subprocess.run(['vlc',video_list[0],'--play-and-exit'])
+                                os.system('python3 /home/pi/Rocket-dashboard/start-main-program.py ' + str(durationInSeconds))
+                                os.system('vlc ' + video_list[0] + ' --play-and-exit')
+                                pygame.quit()
+                                quit()
+  #                              time.sleep(durationInSeconds)
+                                
+   #                             pygame.init()
+    #                            screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+     #                           font = pygame.font.SysFont(None, 66)
+      #                          font2 = pygame.font.SysFont(None, 50)
+#
+ #                               screen.blit(menucontrol, (0,0))
+  #                              screenstate = 0
+
+                        pygame.display.update()
+                        time.sleep(0.5)
+                ### Button2: Cycle
+                if GPIO.input(button2) == False:
+                        bloop.play()
+                        print('b2 pressed')
+                        if GPIO.input(button1) == False:
+                                print('b2-1 pressed')
+                                pygame.quit()
+                                quit()
+                        elif screenstate == 0:	# If on control menu screen, switch to radio menu
+                                screen.blit(menuradio, (0,0))
+                                screenstate = 1
+                        elif screenstate == 1:	# If on radio menu scree, switch to control menu
+                                screen.blit(menucontrol, (0,0))
+                                screenstate = 0
+                        elif screenstate == 2:	# If on dashboard, cycle windows
+                                current_window = current_window+1
+                                if current_window == len(window_list):
+                                        current_window = 0
+                                screen.blit(window_list[current_window], (30,7))
+                                print(window_list[current_window])
+                        elif screenstate == 3: # If on video select screen
+                                ### Rotate video order and color
+                                print('Cycle video list')
+                                video_list = (video_list[1], video_list[2], video_list[3], video_list[0])
+                                color_list = (color_list[1], color_list[2], color_list[3], color_list[0])
+                                print(video_list)
+                                screen.fill((10,10,10))
+                                text1 = font.render(video_list[0].split('/')[-1], True, color_list[0])
+                                text2 = font2.render(video_list[1].split('/')[-1], True, color_list[1])
+                                text3 = font2.render(video_list[2].split('/')[-1], True, color_list[2])
+                                text4 = font2.render(video_list[3].split('/')[-1], True, color_list[3])
+                                screen.blit(text1, (30, 50))
+                                screen.blit(text2, (60, 160))
+                                screen.blit(text3, (60, 260))
+                                screen.blit(text4, (60, 360))
+                                pygame.draw.rect(screen, (230,230,200), (10,10,640,130), 9)
+
+                        else:
+                                pass
+                        pygame.display.update()
+                        time.sleep(0.5)
+                ### Switch1: Light 1
+                if switch1state != GPIO.input(switch1):
+                         beep2.play()
+                         print('switch1 thrown')
+#                         GPIO.output(led1, switch1state)
+                         if switch1state == True:
+                                 light1color = (255,94,0)  # Safety Orange
+                         else:
+                                 light1color = (255,231,184) # Peach
+                         if screenstate == 2:
+                                 pygame.draw.circle(screen, (light1color), (130,370), 40)
+                                 pygame.draw.circle(screen, (150,150,150), (130,370), 42, 6)
+
+                                 print("Updating because we're on screenstate2")
+                                 pygame.display.update()
+                         switch1state = GPIO.input(switch1)
+                ### Switch2: Light 2
+                if GPIO.input(switch2) != switch2state:
+                         biz.play()
+                         print('Switch2 thrown')
+#                         GPIO.output(led1, switch1state)
+                         if switch2state == True:
+                                 light2color = (68,0,255)
+#                                 pygame.draw.circle(screen, (208,255,230), (370,370), 40)
+                         else:
+                                 light2color = (201,212,255)
+#                                 pygame.draw.circle(screen, (10, 255, 15), (370,370), 40)
+#                         pygame.draw.circle(screen, (0, 255, 10), (370,370), 41, 6)
+                         if screenstate == 2:
+                                 pygame.draw.circle(screen, light2color, (370,370), 40)
+                                 pygame.draw.circle(screen, (100,100,100), (370,370), 42, 6)
+                                 pygame.display.update()
+                         switch2state = GPIO.input(switch2)
+
+                ### Switch3
+                if GPIO.input(switch3) != switch3state:
+                         boop.play()
+                         print('Switch3 thrown')
+                         if switch3state == True:
+                                 switch3color = (10,255,15)
+#                                 pygame.draw.circle(screen, (208,255,230), (610,370), 40)
+                         else:
+ #                                pygame.draw.circle(screen, (10, 255, 15), (610,370), 40)
+                                  switch3color = (208, 255, 230)
+                         pygame.draw.circle(screen, (switch3color), (610,370), 40)
+                         pygame.draw.circle(screen, (0, 255, 10), (610,370), 41, 6)
+                         if screenstate == 2:
+                                 pygame.display.update()
+                         switch3state = GPIO.input(switch3)
+                ### Switch4
+                if GPIO.input(switch4) != switch4state:
+                         bip.play()
+                         print('Switch4 thrown!')
+                         if switch4state == True:
+                                 switch4color = (230,  0,  0)
+#                                 pygame.draw.rect(screen, (20,20,255), (50,300,650,150), 16)
+                         else:
+                                 switch4color = (255,150,130)
+ #                                pygame.draw.rect(screen, (200,200,255), (50,300,650,150), 16)
+                         if screenstate == 2:
+                                 print('Updating to new color')
+                                 pygame.draw.rect(screen, (switch4color), (50,300,640,150), 16)
+                                 pygame.display.update()
+                         time.sleep(0.5)
+                         switch4state = GPIO.input(switch4)
+                ### Button3
+#                if GPIO.input(button3) == False:
+ #                        if rollingvalue == 254:
+  #                               modifier = -1
+   #                      if rollingvalue == 5:
+    #                             modifier = 1
+     #                    rollingvalue = rollingvalue + modifier
+      #                   button3color = (10,rollingvalue,0)
+       #                  pygame.draw.polygon(screen, button3color, [(600,150),(680,200),(680,100)])
+        #                 pygame.display.update()
+
+#                         pygame.draw.rect(screen, (200,200,255), (100,300,600,150), 6)
+
+  #              else:
+ #                       pass
+#        pass
+# Import and initialize the pygame library
+# Set up the drawing window
+#pygame.quit()
+except:
+        GPIO.cleanup()
+        print('exception called?')
+        raise
+#        quit()
--- upload_videos.py
+++ upload_videos.py
@@ -8,30 +8,36 @@ import time
 ### Main program
 
 print('Checking for new videos')
-## List local videos
 
+### List local videos
 vid_local = os.listdir('/home/pi/Rocket-dashboard/Videos/')
 
+### Create list of remote videos
 os.system('rclone lsf test5:/mobile_one/>/home/pi/Rocket-dashboard/new_videos')
-
 new_videos = open('/home/pi/Rocket-dashboard/new_videos','r')
 vid_remote = new_videos.read()
 vid_remote = vid_remote.strip().split('\n')
-print(vid_remote)
-
 
 print('Local videos:')
 print(vid_local)
 print('Remote videos:')
 print(vid_remote)
 
-difference = set(vid_remote) - set(vid_local)
+### Define difference sets
+difference_add = set(vid_remote) - set(vid_local)
+difference_remove = set(vid_local) - set(vid_remote)
 
-print('Files to be uploaded')
-print(difference)
+#print('Difference to add:')
+#print(difference_add)
+#print('Difference to remove')
+#print(difference_remove)
 
-if len(difference) >0:	# If there are any files in the remote folder not in the local folder
-	for video in difference:
+### Upload new videos
+print('Files to be uploaded:')
+print(difference_add)
+
+if len(difference_add) >0:	# If there are any files in the remote folder not in the local folder
+	for video in difference_add:
 		print(video)
 #		print('rclone copy test5:/mobile_one/'+ video + ' /home/pi/Rocket-dashboard/Video')
 		os.system('rclone copy test5:/mobile_one/'+ video + ' /home/pi/Rocket-dashboard/Videos')
@@ -39,6 +45,15 @@ if len(difference) >0:	# If there are any files in the remote folder not in the 
 
 print('Uploads complete')
 
+"""
+### Move old videos
+if len(difference_remove) >0:
+	for video in difference_remove:
+		print(video)
+		os.system('mv /home/pi/Rocket-dashboard/Videos/' + video + ' /home/pi/Rocket-dashboard/Videos-old')
+
+print('Video transfer complete')
+"""
 """
 path = "/home/pi/Rocket-dashboard/Sounds"
 files_sorted_by_date = []
